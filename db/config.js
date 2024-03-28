ae42bcd3237442acb95c5e32a532cdc99af07f0b
Hassan <malikhassanhu55@gmail.com>
2024-03-28 22:58:11 +0500

    db

---
+++ db/config.js
@@ -0,0 +1,5 @@
+const mongoose =require("mongoose")
+
+let uri="mongodb://malikhassanhu55:hassan4518@ac-nur02xp-shard-00-00.brb9qki.mongodb.net:27017,ac-nur02xp-shard-00-01.brb9qki.mongodb.net:27017,ac-nur02xp-shard-00-02.brb9qki.mongodb.net:27017/?ssl=true&replicaSet=atlas-m47e0y-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(uri)
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app=express();
+const cors =require("cors");
+
+require("./db/config")
+let User=require("./db/user")
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("api is working")
+})
+
+app.post("/signup", async (req, resp) => {
+    let user = User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    resp.send(result);
+  });
+
+
+
+app.listen(5000)
